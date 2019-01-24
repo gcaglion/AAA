9280cff895245c2790d5dc7b2883e8ddc90902c7
Giacomo Caglioni <gcaglion@yahoo.com>
2019-01-25 00:07:08 +0100

    added ETXEUR and WTIUSD to AlgoDB_Management

--- AlgoDB/ExplainPlan.sql
+++ AlgoDB/ExplainPlan.sql
@@ -2,4 +2,4 @@ explain plan for delete from MYLOG_MSE where processid in(select processid from(
 SELECT PLAN_TABLE_OUTPUT FROM TABLE(DBMS_XPLAN.DISPLAY(NULL));
 
 -- this runs EXPLAIN PLAN for a running statement:
-select plan_table_output from v$sql s, table(dbms_xplan.display_cursor(s.sql_id, s.child_number, 'basic')) t where s.sql_id='1bugu0axu7j32'
+select plan_table_output from v$sql s, table(dbms_xplan.display_cursor(s.sql_id, s.child_number, 'basic')) t where s.sql_id='&1'
