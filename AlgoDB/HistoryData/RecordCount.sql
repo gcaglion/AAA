2e4e2993635c2735d4dd18d0155b28fb4bc78c8b
Giacomo <gcaglion@yahoo.com>
2020-09-07 17:14:15 +0200

    Added scripts for AUDNZD pair

--- AlgoDB/HistoryData/RecordCount.sql
+++ AlgoDB/HistoryData/RecordCount.sql
@@ -33,7 +33,7 @@ commit;
 
 drop type RecordCount_tab;
 drop type RecordCount_obj;
-create type RecordCount_obj is object(TableName varchar2(64), Symbol varchar2(12), TimeFrame varchar2(3), Filled varchar2(6), TotalCount number, minDate date, maxDate date);
+create type RecordCount_obj is object(TableName varchar2(64), Symbol varchar2(12), TimeFrame varchar2(6), Filled varchar2(6), TotalCount number, minDate date, maxDate date);
 /
 show errors;
 create type RecordCount_tab is table of RecordCount_obj;
@@ -56,14 +56,14 @@ BEGIN
 					) TimeFrame
 				from user_tables where table_name<>'CATEGORIES' order by 1
 			) loop
-		--dbms_output.put_line('select count(*), min(NewDateTime), max(NewDateTime) from '||r.table_name);
+		dbms_output.put_line('select count(*), min(NewDateTime), max(NewDateTime) from '||r.table_name);
 		execute immediate 'select count(*), min(NewDateTime), max(NewDateTime) from '||r.table_name into vCount, vMinDate, vMaxDate;
 		l_RecordCount_tab.extend;
 		n:=n+1;
-		--dbms_output.put_line('r.BaseTable=***'||r.BaseTable||'***');
-		--dbms_output.put_line('r.TimeFrame=***'||r.TimeFrame||'***');
+		dbms_output.put_line('r.BaseTable=***'||r.BaseTable||'***');
+		dbms_output.put_line('r.TimeFrame=***'||r.TimeFrame||'***');
 		l_RecordCount_tab(n) := RecordCount_obj(r.table_name, r.BaseTable, r.TimeFrame, r.TableType, vCount, vMinDate, vMaxDate);
-		--dbms_output.put_line('select count(*), min(NewDateTime), max(NewDateTime) from '||r.table_name);
+		dbms_output.put_line('select count(*), min(NewDateTime), max(NewDateTime) from '||r.table_name);
 /*
 		execute immediate 'select count(*), min(NewDateTime), max(NewDateTime) from '||r.table_name||'_FILLED' into vCount, vMinDate, vMaxDate;
 		l_RecordCount_tab.extend;
